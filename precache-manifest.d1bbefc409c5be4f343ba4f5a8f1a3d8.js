2fc2fcb78fd1be951a5f2b66f30bc8c3f5a85398
herkar95 <herman.karlsson@hotmail.se>
2020-08-18 20:20:21 +0200

    Updates

---
+++ precache-manifest.d1bbefc409c5be4f343ba4f5a8f1a3d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edef2e93a5944a5bf250f11fabd47a90",
+    "url": "/basic-ecommerce/index.html"
+  },
+  {
+    "revision": "429be0d6b97665f7eb47",
+    "url": "/basic-ecommerce/static/css/main.739b9d24.chunk.css"
+  },
+  {
+    "revision": "3f9c91e02b81305c5fec",
+    "url": "/basic-ecommerce/static/js/2.31bb2651.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/basic-ecommerce/static/js/2.31bb2651.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "429be0d6b97665f7eb47",
+    "url": "/basic-ecommerce/static/js/main.9455fac7.chunk.js"
+  },
+  {
+    "revision": "aa516aa9f663e3c4fd45",
+    "url": "/basic-ecommerce/static/js/runtime-main.1a6fb5d6.js"
+  }
+]);
